d6129450191bb564ea7a686e22ff63a79b484b06
Stina Qvarnström <stina.qvarnstrom@jayway.com>
2022-10-12 20:58:38 +0200

    Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import DisplayCost from './features/DisplayCost/display-cost';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <DisplayCost watt={0.3} possessivePronoun={'Min'} thing={'glödlampa'} costPerKWh={2222}></DisplayCost>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/error" element={<Error />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Main() {
+  return (
+    <div>
+      <DisplayCost watt={0.3} possessivePronoun={'Min'} thing={'glödlampa'} costPerKWh={2222}></DisplayCost>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Error() {
+  return (
+    <div>
+      <h2>Oh no, something went wrong</h2>
+    </div>
+  );
+}
