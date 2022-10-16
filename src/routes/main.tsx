aad710aad137829c6ad4f56d1958e26af2d60595
Stina Qvarnström <stina.qvarnstrom@jayway.com>
2022-10-16 21:06:09 +0200

    Sort of works with user input for watt and öre

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import DisplayCost from './../features/DisplayCost/display-cost';
 import InputValues from './../features/InputValues/input-values';
 
 function Main() {
+  const [feeKrPerKWh, setFeeKrPerKWh] = useState(0);
+  const [kiloWatt, setKiloWatt] = useState(0);
+
+  const handleKiloWattChange = (newValue: number) => {
+    setKiloWatt(newValue);
+  };
+
+  const handleFeeKrPerKWh = (newValue: number) => {
+    setFeeKrPerKWh(newValue);
+  };
+
   return (
     <div>
-      <DisplayCost kW={2000/1000} possessivePronoun={'Min'} thing={'vattenkokare'} feeKrPerKWh={480/100} />
-      {/* <InputValues /> */}
+      <DisplayCost kW={kiloWatt} possessivePronoun={'Min'} thing={'vattenkokare'} feeKrPerKWh={feeKrPerKWh/100} />
+      <InputValues
+        kiloWatt={kiloWatt}
+        onKiloWattChange={handleKiloWattChange}
+        feeKrPerKWh={feeKrPerKWh}
+        onFeeKrPerKWhChange={handleFeeKrPerKWh} />
     </div>
   );
 }
